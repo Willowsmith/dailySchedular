d0a8398b52fd9b2b65f7cdf7c7533c5aedd265bd
Willowsmith <willowrosedennis@gmail.com>
2021-10-19 14:00:22 -0700

    Fixed a load of major issues, appears to be working

--- script.js
+++ script.js
@@ -1,19 +1,47 @@
 var currentday = $('#currentDay');
 var scheduleDisplayEl = $('#scheduleDisplay')
-var h9 = $('h9');
-var h9 = $('h10');
-var h9 = $('h11');
-var h9 = $('h12');
-var h9 = $('h13');
-var h9 = $('h14');
-var h9 = $('h15');
-var h9 = $('h16');
-var h9 = $('h17');
-var saveBtn = $('saveBtn');
+var hour = $('.hour');
+var h9 = $('#9');
+var h9 = $('#10');
+var h9 = $('#11');
+var h9 = $('#12');
+var h9 = $('#13');
+var h9 = $('#14');
+var h9 = $('#15');
+var h9 = $('#16');
+var h9 = $('#17');
+var saveBtn = $('.saveBtn');
 
+var currentDay = moment();
+$("#currentDay").text(currentDay.format("MMM Do, YYYY"));
 
+var time = moment().format("H");
 
-function displayTime() {
-    var rightNow = moment().format('dddd, MMMM do');
-    currentday.text(rightNow);
+function tasks() {
+  for (let i = 9; i <= 17; i++) {
+      var getTask = localStorage.getItem(i);
+      $('#' + i).val(getTask);
   }
+}
+
+function color() {
+    for (let hour = 9; hour <= 17; hour++) {
+        console.log(hour)
+        if (hour == time) {
+            $("#" + hour).addClass("present");
+        } else if (hour < time) {
+            $("#" + hour).addClass("past")
+        } else if (hour > time) {
+            $("#" + hour).addClass("future")
+        }
+    }
+}
+
+saveBtn.click(function(event) {
+  var task = $(event.target).prev().val();
+  var hourID = $(event.target).prev().attr("id")
+  localStorage.setItem(hourID, task)
+})
+
+color();
+tasks();
